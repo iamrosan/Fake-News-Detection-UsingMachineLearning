f34c3f3c246c94b98465b64c629422bb29f037e6
roobeen-b <rubinbaidhya@gmail.com>
2022-02-19 10:06:58 +0545

    Added model in views-py

--- FakeNewsDetection/articlePrediction/views.py
+++ FakeNewsDetection/articlePrediction/views.py
@@ -1,9 +1,12 @@
+import pickle
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import scraping
+from keras.preprocessing import text,sequence
+
 # from tensorflow.keras.preprocessing.sequence import pad_sequences
 # from tensorflow.keras.models import Sequential
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 # tfidf_vectorizer=TfidfVectorizer(stop_words='english')
 # data=tfidf_vectorizer.fit_transform(data) 
 # X_test=tfidf_vectorizer.transform(X_test)
@@ -27,19 +30,30 @@ def home(request):
             'values': value
         }
         print(context)
+
+
+
+
+    with open('./model1.pkl','rb') as md:
+        lstmModel = pickle.load(md)
+
     news = "Washington Sundar has been ruled out of the Twenty20 series against West Indies. The Chennai-based Indian spinner, who staged an international comeback in the recent ODI series against West Indies after a long jury-forced hiatus, will require a few weeks to recover, it is learned.  Washington Sundar suffered a left hamstring muscle strain during fielding in the third ODI, said a statement from BCCI. Kuldeep Yadav has been named as his replacement.Washington will have to report at the NCA on Tuesday (February 15) and is set to spend three weeks at the NCA.   The three Twenty20 Internationals against West Indies will start in Kolkata on February 16.   Sundar left the Indian camp that is currently in Kolkata. The Indian team had a net session at Eden Gardens in Kolkata on Monday (February 14) evening."
     # scraping.printArticle(news)
-    # tfidf_vectorizer=TfidfVectorizer(stop_words='english')
-    # data=tfidf_vectorizer.fit_transform(data) 
-    # X_test=tfidf_vectorizer.transform(X_test)
-    # tokenized_test = tokenizer.texts_to_sequences(news)
-    # li = sequence.pad_sequences(tokenized_test, maxlen=max_len)
-    # with open('model1.pkl','rb') as md:
-    #     lstmModel = pickle.load(md)
+
+    max_features = 10000
+    max_len = 300
+
+    tokenizer = text.Tokenizer(num_words=max_features)
+
+    tokenized_test = tokenizer.texts_to_sequences(news)
+    news = sequence.pad_sequences(tokenized_test, maxlen=max_len)
     
-    # value = lstmModel.predict(news)
-    # context={
-    #         'values': value
-    #     }
+    # tfidf_vectorizer=TfidfVectorizer(stop_words='english')
+    # news = tfidf_vectorizer.transform(news)
+    
+    value = lstmModel.predict(news)
+    context={
+            'values': value
+        }
     # print(urlll)
     return render(request, 'index.html', context)
