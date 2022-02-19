600f2db231d8eab4c775183ae2e2a16c89d5d6dc
roobeen-b <rubinbaidhya@gmail.com>
2022-02-19 16:58:01 +0545

    Modified views-py

--- FakeNewsDetection/articlePrediction/views.py
+++ FakeNewsDetection/articlePrediction/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import scraping
 from keras.preprocessing import text,sequence
+import tensorflow as tf
 
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 # from tensorflow.keras.models import Sequential
 from sklearn.feature_extraction.text import TfidfVectorizer
 # tfidf_vectorizer=TfidfVectorizer(stop_words='english')
@@ -34,8 +35,10 @@ def home(request):
 
 
 
-    with open('./model1.pkl','rb') as md:
-        lstmModel = pickle.load(md)
+    # with open('E:\RB\Fake-News-Detection-BCT-Mini-Project-\FakeNewsDetection\model.pkl','rb') as md:
+    #     lstmModel = pickle.load(md)
+
+    lstmModel = tf.keras.models.load_model('E:\RB\Fake-News-Detection-BCT-Mini-Project-\saved_model\lstmModel')
 
     news = "Washington Sundar has been ruled out of the Twenty20 series against West Indies. The Chennai-based Indian spinner, who staged an international comeback in the recent ODI series against West Indies after a long jury-forced hiatus, will require a few weeks to recover, it is learned.  Washington Sundar suffered a left hamstring muscle strain during fielding in the third ODI, said a statement from BCCI. Kuldeep Yadav has been named as his replacement.Washington will have to report at the NCA on Tuesday (February 15) and is set to spend three weeks at the NCA.   The three Twenty20 Internationals against West Indies will start in Kolkata on February 16.   Sundar left the Indian camp that is currently in Kolkata. The Indian team had a net session at Eden Gardens in Kolkata on Monday (February 14) evening."
     # scraping.printArticle(news)
@@ -57,3 +60,22 @@ def home(request):
         }
     # print(urlll)
     return render(request, 'index.html', context)
+
+def index(request):
+    lstmModel = tf.keras.models.load_model('E:\RB\Fake-News-Detection-BCT-Mini-Project-\saved_model\lstmModel')
+
+    news = "Washington Sundar has been ruled out of the Twenty20 series against West Indies. The Chennai-based Indian spinner, who staged an international comeback in the recent ODI series against West Indies after a long jury-forced hiatus, will require a few weeks to recover, it is learned.  Washington Sundar suffered a left hamstring muscle strain during fielding in the third ODI, said a statement from BCCI. Kuldeep Yadav has been named as his replacement.Washington will have to report at the NCA on Tuesday (February 15) and is set to spend three weeks at the NCA.   The three Twenty20 Internationals against West Indies will start in Kolkata on February 16.   Sundar left the Indian camp that is currently in Kolkata. The Indian team had a net session at Eden Gardens in Kolkata on Monday (February 14) evening."
+
+    max_features = 10000
+    max_len = 300
+
+    tokenizer = text.Tokenizer(num_words=max_features)
+
+    tokenized_test = tokenizer.texts_to_sequences(news)
+    news = sequence.pad_sequences(tokenized_test, maxlen=max_len)
+    
+    value = lstmModel.predict(news)
+    context={
+            'values': value
+        }
+    return render(request, 'index2.html', context)
