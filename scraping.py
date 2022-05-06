1d3a6f2beb2b15a1e22407291714aa5c31080985
iamrosan <roshanshrestha21.63@gmail.com>
2022-05-06 19:10:23 +0545

    final commit to web scrap

--- scraping.py
+++ scraping.py
@@ -1,6 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
+#guardian -real
+url = [
+    'https://thehimalayantimes.com/nepal/uml-rival-factions-in-war-of-words',
+    'https://thehimalayantimes.com/nepal/oli-firm-on-decision-to-amend-uml-statute',
+    'https://thehimalayantimes.com/business/pm-must-seek-trust-vote-by-april-5-cpn-mc',
+    'https://thehimalayantimes.com/nepal/nepali-congress-demands-action-against-rampur-incident-culprits',
+    'https://thehimalayantimes.com/nepal/five-parties-in-na-by-election-fray',
+    'https://thehimalayantimes.com/nepal/na-by-poll-voting-ends-337-cast-votes-10-absent',
+    'https://thehimalayantimes.com/nepal/nea-cut-power-supply-to-international-cricket-stadium',
+    'https://thehimalayantimes.com/nepal/provincial-hospitals-service-to-be-improved',
+    'https://thehimalayantimes.com/nepal/bagmati-provincial-govt-splits-ministries',
+    'https://thehimalayantimes.com/nepal/rural-municipality-chair-26-others-booked-for-graft',
+    'https://thehimalayantimes.com/nepal/conspiracy-being-hatched-to-break-alliance-pm-deuba',
+    'https://thehimalayantimes.com/nepal/nepal-records-10-covid-19-cases-on-tuesday',
+    'https://thehimalayantimes.com/nepal/nepals-top-court-gives-temporary-reprieve-to-central-bank-governor',
+    
+]
+
 # cnn
 # url = 'https://edition.cnn.com/2022/02/10/politics/donald-trump-gop-incumbents-impeach-votes/index.html' 
 
@@ -32,27 +50,30 @@ import requests
 # url = 'https://www.theguardian.com/football/2022/feb/10/chelsea-braced-for-kepa-arrizabalaga-bids-and-open-to-summer-exit'
 
 #abc news
-url = 'https://abcnews.go.com/Politics/pressure-builds-biden-democrats-move-past-covid/story?id=82754983'
+# url = 'https://abcnews.go.com/Politics/pressure-builds-biden-democrats-move-past-covid/story?id=82754983'
 
 # url = 'https://edition.cnn.com/2022/02/10/politics/biden-ukraine-things-could-crazy/index.html'
-def getUrl(url):
+
+
+def getUrl(url): #get the response 
     pageContent = requests.get(url)
     print(pageContent)
     return pageContent
 
 def parse(pagecontent):
-    if pagecontent.status_code == 404:
+    data = []
+    if pagecontent.status_code != 200: #beside response 200 for all response show page not found
         print('Page not found')
         return 0
-    coup = BeautifulSoup(pagecontent.content, 'html.parser')
+    coup = BeautifulSoup(pagecontent.content, 'html.parser') #Parse the content using bs4
     try:
-        if coup.find('article') is not None:
+        if coup.find('article') is not None: #if news article is in article tag
             print('article')
             contentParse = coup.find('article')
-        # elif coup.find('section') is not None:
-        #     print('section')
-            # contentParse = coup.find('section')
-        elif coup.find('div') is not None:
+
+        
+
+        elif coup.find('div') is not None: #if news article is in div tag
             print("div")
             #searching for the right div is left here
             contentParses = coup.find_all('div')
@@ -63,36 +84,69 @@ def parse(pagecontent):
                 if contentParse.find('h1') is not None:
                     # print('xir3')
                     headline = contentParse.find('h1').text
-                    print(f'Title: {headline}')
+                    # print(f'Title: {headline}')
                     break
                 flag +=1
             count = 0
             newsArticles = contentParses[flag].find_all('p')
             for newsArticle in newsArticles:
-                if count == 3:
+                if count == 5:
                     break
-                print(newsArticle.text, end=' ')
+                # print(newsArticle.text, end=' ')
+                data.append(newsArticle.text)
                 count +=1
-            return 1
+            data.insert(0, headline)
+            data = ' '.join(data)
+            dictt = {
+            # 'title': headline,
+            'article': data
+        }
+            print(dictt)
+            return dictt # to process only the article which is in div tag
         else:
             errormessage = 'No content found'
             print(errormessage)
         
         # print('teha gayana')
         # print(contentParse)
-
+# to process the article which is in article tag
         headline = contentParse.find('h1').text
-        print(f"Title: {headline}")
+        # print(f"Title: {headline}")
 
         newsArticles = contentParse.find_all('p')
     # print(len(newsArticles))
 
         for newsArticle in newsArticles:
-            print(newsArticle.text, end=' ')
-    
-    except:
+            # print(newsArticle.text, end=' ')
+            data.append(newsArticle.text)
+        # dictt = {
+        #     'title': headline,
+        #     'article': data
+        # }
+        data.insert(0, headline)
+        data = ' '.join(data)
+        dictt = {
+            'title': headline,
+            'content': data
+        }
+
+        print(dictt)
+        return dictt
+
+    except: #any error occured during the process 
         print("Error Occured")
 
-
-
-parse(getUrl(url))
+import csv
+fieldnames = ['title','content']
+with open('news.csv', 'a', newline='') as csv_file:
+    
+    # csv_writer = csv.writer(csv_file)
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    for lin in url:
+        news = parse(getUrl(lin))
+    # print()
+    # print('printing for csv file: {}'.format(news['title']))
+    # print()
+    # print('printing for csv file: {}'.format(news['content']))
+    # csv_writer.writeheader()   #---> for setting up the header for the csv file
+        csv_writer.writerow(news)
